4d04877b915ba8886df713c43a8a26907cd5b668
Luis Eduardo <luisedumlg@gmail.com>
2020-05-18 11:57:39 -0300

    Redefinição dos arquivos head, inferior, middle, superior

--- src/assets/character/Character.js
+++ src/assets/character/Character.js
@@ -1,51 +1,78 @@
-function createCharacter(gender, skinColor, hairColor, eyeColor, mouthColor, bodyColor, legColor, shoeColor) {
-    var character = new THREE.Group();    
+const createHead = require('./createHead');
+const createInferior = require('./createInferior');
+const createMiddle = require('./createMiddle');
+const createSuperior = require('./createSuperior');
 
-    // Criação dos membros inferiores
+class Character {
+   
+   constructor(gender, skinColor, hairColor, eyeColor, mouthColor, bodyColor, legColor, shoeColor) {
+      this.entity = new THREE.Group();    
 
-    var inferiorLeft = createInferior(legColor, shoeColor);
-    inferiorLeft.position.set(1.75, 4, 0);
-    inferiorLeft.name = "inferiorLeft";
+      // Criação dos membros inferiores
 
-    var inferiorRight = createInferior(legColor, shoeColor);
-    inferiorRight.position.set(-1.75, 4, 0);
-    inferiorRight.name = "inferiorRight";
+      this.inferiorLeft = createInferior(legColor, shoeColor);
+      this.inferiorLeft.position.set(1.75, 4, 0);
+      this.inferiorLeft.name = "inferiorLeft";
 
-    var inferior = new THREE.Group();
-    inferior.add(inferiorLeft);
-    inferior.add(inferiorRight);
-    inferior.name = "inferior";
-    character.add(inferior);
+      this.inferiorRight = createInferior(legColor, shoeColor);
+      this.inferiorRight.position.set(-1.75, 4, 0);
+      this.inferiorRight.name = "inferiorRight";
 
-    // Criação do meio
+      this.inferior = new THREE.Group();
+      this.inferior.add(inferiorLeft);
+      this.inferior.add(inferiorRight);
+      this.inferior.name = "inferior";
+      this.character.add(inferior);
 
-    var middle = createMiddle(gender, bodyColor, legColor, hairColor);
-    middle.position.set(0, 12, 0);
-    middle.name = "middle";
-    character.add(middle);
+      // Criação do meio
 
-    // Criação dos membros superiores
+      this.middle = createMiddle(gender, bodyColor, legColor, hairColor);
+      this.middle.position.set(0, 12, 0);
+      this.middle.name = "middle";
+      this.character.add(middle);
 
-    var superiorLeft = createSuperior(skinColor, bodyColor);
-    superiorLeft.position.set(4.25, 12, 0);
-    superiorLeft.name = "superiorLeft";
+      // Criação dos membros superiores
 
-    var superiorRight = createSuperior(skinColor, bodyColor);
-    superiorRight.position.set(-4.25, 12, 0);
-    superiorRight.name = "superiorRight";
+      this.superiorLeft = createSuperior(skinColor, bodyColor);
+      this.superiorLeft.position.set(4.25, 12, 0);
+      this.superiorLeft.name = "superiorLeft";
 
-    var superior = new THREE.Group();
-    superior.add(superiorLeft);
-    superior.add(superiorRight);
-    superior.name = "superior";
-    character.add(superior);
+      this.superiorRight = createSuperior(skinColor, bodyColor);
+      this.superiorRight.position.set(-4.25, 12, 0);
+      this.superiorRight.name = "superiorRight";
 
-    // Criação da cabeça
+      this.superior = new THREE.Group();
+      this.superior.add(superiorLeft);
+      this.superior.add(superiorRight);
+      this.superior.name = "superior";
+      this.character.add(superior);
 
-    var head = createHead(gender, skinColor, hairColor, eyeColor, mouthColor);
-    head.position.set(0, 18, 0);
-    head.name = "head";
-    character.add(head);
+      // Criação da cabeça
 
-    return character;
+      this.head = createHead(gender, skinColor, hairColor, eyeColor, mouthColor);
+      this.head.position.set(0, 18, 0);
+      this.head.name = "head";
+      this.character.add(head);
+
+      this.forearmUp = false;
+
+   }
+
+   moveForearm(){
+      var forearm = this.superiorRight.getObjectByName("forearm");
+      var arm = this.superiorRight.getObjectByName("arm");
+  
+      if(this.forearmUp) {
+         forearm.translateY(0.01);
+         forearm.translateZ(0.01);
+  
+         if(forearm.position.y > arm.position.y) this.forearmUp = false;
+      } else{
+         forearm.translateY(-0.01);
+         forearm.translateZ(-0.01);
+  
+         if(forearm.position.z < arm.position.z) this.forearmUp = true;
+      }
+   }   
 }
+module.exports = Character;
